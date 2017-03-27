c439c8df48d3a8dea7b645680957ce0d8ff76763
Alessio Pinato <a.pinato@olsalan.ifrst>
2017-03-27 14:59:46 +0200

    second commit

--- README.md
+++ README.md
@@ -2,8 +2,10 @@
 ### A special http client
 
 
+
 1. aggiungendo solo .gitignore non ha funzionato
 2. migliorare il codice e questo file README
+3. seguire l'articolo da qui: (http://pycurl.io/docs/latest/quickstart.html#examining-response-headers)
 
 
 [lista di vari .gitingore](https://github.com/github/gitignore)
--- pythonhttpclient.py
+++ pythonhttpclient.py
@@ -1,3 +1,21 @@
-import urllib.request
-result = urllib.request.urlopen("http://www.google.com").read()
-print(result)
+import pycurl
+from io import BytesIO
+
+#PycURL does not provide storage for the network response, so we need a buffer
+buffer = BytesIO()
+
+c = pycurl.Curl()
+c.setopt(c.VERBOSE, True)
+c.setopt(c.URL, 'www.google.com')
+c.setopt(c.WRITEDATA,buffer)
+c.perform()
+c.close()
+
+body = buffer.getvalue()
+
+# Body is a byte string.
+# We have to know the encoding in order to print it to a text file
+# such as standard output.
+print(body.decode('iso-8859-1'))
+
+#http://pycurl.io/docs/latest/quickstart.html
